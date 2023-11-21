4466586a6012b47fb10ac6e7dfadbd70a4c749a2
bowski43 <43mngt@gmail.com>
2023-11-21 13:42:45 +0300

    refresh after hw check

---
+++ ColorizedApp/SecondVC.swift
@@ -0,0 +1,62 @@
+//
+//  SecondVC.swift
+//  ColorizedApp
+//
+//  Created by Serge Bowski on 11/21/23.
+//
+
+import UIKit
+
+final class SecondVC: UIViewController {
+    
+    @IBOutlet private weak var colorView: UIView!
+    
+    @IBOutlet private weak var redLabel: UILabel!
+    @IBOutlet private weak var greenLabel: UILabel!
+    @IBOutlet private weak var blueLabel: UILabel!
+    
+    @IBOutlet private weak var redSlider: UISlider!
+    @IBOutlet private weak var greenSlider: UISlider!
+    @IBOutlet private weak var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setColor()
+    }
+    
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    private func setColor() {
+        
+        colorView.backgroundColor = UIColor(
+            red: redSlider.value.cgFloat(),
+            green: greenSlider.value.cgFloat(),
+            blue: blueSlider.value.cgFloat(),
+            alpha: 1
+        )
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
+
+
+extension Float {
+    func cgFloat() -> CGFloat {
+        CGFloat(self)
+    }
+}
